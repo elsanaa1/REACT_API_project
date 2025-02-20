e1fde512d58977bb59bea70be2ee1400f0b33d63
elsanaa1 <sanaa.jerbouh1@gmail.com>
2025-02-20 23:41:50 +0100

    first commit

--- src/UserList.js .js
+++ src/UserList.js .js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 export default function UserList ({listOfUSer}) {
+    const divStyle={ margin:'12px auto', backgroundColor:'#2C3930',color:'white', width:'40%',padding:6}
   return (
     <div>
-        {/* {   map list of listOfUSer to display every item inside a div element */
+        {
+        /* {   map list of listOfUSer to display every item inside a div element */
             listOfUSer.map((e)=>(
-                <div style={{ margin:'12px auto', backgroundColor:'#2C3930',color:'white', width:'40%'}} key={e.id}>
-                    <h3>{e.title}</h3>
-                    <p><span>Id: </span>{e.id}</p>
-                    <p> <span>userId: </span>{e.userId}</p>
-                    <p><span>Completed: </span>{e.completed? 'true':'false'}</p>
+                <div style={divStyle} key={e.id}>
+                    <h3>Title: {e.title}</h3>
+                    <p>Id: {e.id}</p>
+                    <p> UserId: {e.userId}</p>
+                    <p>Completed: {e.completed? 'true':'false'}</p>
                 </div>
             ))
-            
+
         }
     </div>
   )
